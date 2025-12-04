7cb169608a4648e71dd08f3a64a56970a86fb675
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-12-04 23:50:19 +0530

    code push

--- Backend/src/db/index.ts
+++ Backend/src/db/index.ts
@@ -19,3 +19,22 @@ const UserSchema: Schema<User> = new Schema({
 // like () || () aiska kuch lete the but in normal node js mai hamm aisa nahi kkarnge yah pp sirf normal hi raehga okkh!..
 
 export const UserModel = mongoose.model<User>("UserSchema", UserSchema);
+
+// dekh bhai tughe like kuch chizo ka dhyan rakhna hai jaise
+// create`isse dataBase m chize jayngi!..
+// find() isse array return hoga okkH! findOne() isse object return hoga okkh!.
+// findById() id s wo ek sigle object return hoga findMany(filter)
+
+// create isse dataBase m chize push ho jayengi okkh!..
+
+// find() isse multiple data return hoga in array okkh!..
+// findOne() s hame ek hi object return hoga onlyy ek hi okkh!..
+// findById()  isse bhi ek ho data return hoga okkh!//
+
+// UpdateOne(filter, {update}):- updateOne({email:'parvejkaahn@123'}, {age:22}) isme 5 user ka email same hai but fir bhi first ka upadte hoga okkh!.
+// UpdateMany():- upadteOne({email:'parvejkaahn@123'}, {age:22}) isme 5 agar same email k mil gaye to wo sare upadte honge okkh!..
+
+// deleteOne(filter):- isse ek match wala hi delete hoga but sirf ek hi objcet okkh!.
+//deleteMany(filter):- isse match karke sara object delete kar dega p=okkh!.
+// delereOneAndupdate(fikter):-- pehle find karega and delete kar deg wo!
+//delteteByIdAndUpdate(filter):= ye pehhle id s find karega then delete kard dega okkh!..
